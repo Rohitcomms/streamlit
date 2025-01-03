71d16750fbe7751d80be2332a148da93bcd35e41
Rohitcomms <rohitchennuri2@gmail.com>
2025-01-03 09:11:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ url = st.text_input("Enter YouTube URL:")
 
 if st.button("Get Summary"):
     if url:
-        backend_url = "https://dashboard.render.com/web/srv-ctrl7vlumphs73dvbu1g/deploys/dep-ctrl7vtumphs73dvbu60/summarize" 
+        backend_url = "https://flsk-2.onrender.com/summarize"
         response = requests.post(backend_url, json={"url": url})
         
         if response.status_code == 200:
@@ -17,4 +17,5 @@ if st.button("Get Summary"):
             st.error(response.json().get("error", "An error occurred."))
     else:
         st.error("Please enter a valid URL.")
+
 
