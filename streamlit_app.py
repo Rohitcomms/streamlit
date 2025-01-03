ccbcfec11c4b288ad17c20251e891a890803586b
Rohitcomms <rohitchennuri2@gmail.com>
2025-01-03 08:11:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import requests
-
 st.title("YouTube Video Transcript Summarizer")
 
 url = st.text_input("Enter YouTube URL:")
 
 if st.button("Get Summary"):
     if url:
-        response = requests.post("https://dashboard.render.com/web/srv-ctrke33tq21c738tec8g/deploys/dep-ctrke3btq21c738tecb0", json={"url": url})
+        # Replace with your Flask app's public URL
+        response = requests.post("https://srv-ctrkp13tq21c738tjl1g.onrender.com/summarize", json={"youtube_url": url})
+        
         if response.status_code == 200:
             st.subheader("Summary:")
-            st.write(response.json()["summary"])
+            st.write(response.json()["summary"])  
         else:
             st.error(response.json().get("error", "An error occurred."))
     else:
-        st.error("Please enter a valid URL.")
+        st.error("Please enter a valid YouTube URL.")
 
