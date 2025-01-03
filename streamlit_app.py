634657852394dc71fc41568bd0c59c00bebf9c3c
Rohitcomms <rohitchennuri2@gmail.com>
2025-01-03 09:40:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,21 @@ url = st.text_input("Enter YouTube URL:")
 
 if st.button("Get Summary"):
     if url:
-        backend_url = "https://flsk-2.onrender.com/summarize"
-        response = requests.post(backend_url, json={"url": url})
-        
-        if response.status_code == 200:
-            st.subheader("Summary:")
-            st.write(response.json()["summary"])
-        else:
-            st.error(response.json().get("error", "An error occurred."))
+        backend_url = "http://127.0.0.1:10000/summarize" 
+        try:
+            
+            response = requests.post(backend_url, json={"url": url})
+
+            if response.status_code == 200:
+                st.subheader("Summary:")
+                st.write(response.json()["summary"])
+            else:
+            
+                st.error(response.json().get("error", "An error occurred."))
+        except requests.exceptions.RequestException as e:
+           
+            st.error(f"Network error: {e}")
     else:
-        st.error("Please enter a valid URL.")
+        st.error("Please enter a valid YouTube URL.")
 
 
